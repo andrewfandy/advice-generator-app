5a2394fdf758b5c2523cb1baa180c5502f59bb1e
Andrew Ariel Fandy Tuyu <63329146+andrewfandy@users.noreply.github.com>
2024-04-07 14:07:26 +0700

    Fix mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <>
       <main className='flex flex-col justify-center items-center h-screen w-screen font-family-base font-weight-base'>
-        <div className="p-2 flex flex-col justify-center items-center bg-opacity-30 bg-neutral-gray-blue rounded-md max-w-md min-w-[480px] m-3 h-1/2 relative">
+        <div className="p-2 flex flex-col justify-center items-center bg-opacity-30 bg-neutral-gray-blue rounded-md max-w-md min-w-[28rem] m-3 h-1/2 relative">
           <AdviceMsg />
         </div>
       </main >
